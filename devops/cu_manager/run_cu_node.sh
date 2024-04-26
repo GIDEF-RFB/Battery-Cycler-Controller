48528c618b43c761a39033a51527aa50b0c1b713
RobertoAldea <r.aldea.csic@gmail.com>
2024-04-26 08:21:43 +0200

    Fast solution for bug with cu_id, todo improvements

--- devops/cu_manager/run_cu_node.sh
+++ devops/cu_manager/run_cu_node.sh
@@ -3,6 +3,12 @@
 CU_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd)
 cd ${CU_DIR}/../../
 CONFIG_DIR=$( cd "${CU_DIR}/../../config/" && pwd)
+CU_ID =${CONFIG_DIR}/cu_manager/.cu_id
+if ! [ -f $CU_ID ]; then
+    echo "CU ID file not found"
+else
+    rm $CU_ID
+fi
 export CONFIG_FILE_PATH=${CONFIG_DIR}/config_params.yaml
 python3  -m pip install --upgrade rfb_cycler_cu_manager
 python3  $CU_DIR/run_cu_node.py
